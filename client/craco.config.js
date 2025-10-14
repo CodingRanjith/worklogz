51be83c4228d117e069c4071f23931930aaeb7f1
CodingRanjith <ranjith.c96me@gmail.com>
2025-10-14 17:50:08 +0530

    fix the error

--- client/craco.config.js
+++ client/craco.config.js
@@ -4,6 +4,19 @@ module.exports = {
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
       // Suppress source map warnings for third-party libraries
+      webpackConfig.module.rules.push({
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+        exclude: [
+          // Exclude problematic packages from source map processing
+          /node_modules\/html2pdf\.js/,
+          /node_modules\/jspdf/,
+          /node_modules\/html2canvas/,
+        ],
+      });
+
+      // Also suppress warnings in existing source-map-loader rules
       webpackConfig.module.rules.forEach((rule) => {
         if (rule.use) {
           rule.use.forEach((use) => {
@@ -12,7 +25,8 @@ module.exports = {
               use.options.filterSourceMappingUrl = (url, resourcePath) => {
                 // Suppress warnings for html2pdf.js and other problematic packages
                 if (resourcePath.includes('html2pdf.js') || 
-                    resourcePath.includes('node_modules')) {
+                    resourcePath.includes('html2pdf') ||
+                    resourcePath.includes('es6-promise.map')) {
                   return false;
                 }
                 return true;
@@ -20,6 +34,18 @@ module.exports = {
             }
           });
         }
+      });
+
+      // Remove existing source-map-loader rules that might cause issues
+      webpackConfig.module.rules = webpackConfig.module.rules.filter((rule) => {
+        if (rule.enforce === 'pre' && rule.use) {
+          const hasSourceMapLoader = rule.use.some((use) => 
+            typeof use === 'string' ? use.includes('source-map-loader') : 
+            use.loader && use.loader.includes('source-map-loader')
+          );
+          return !hasSourceMapLoader;
+        }
+        return true;
       });
 
       return webpackConfig;
