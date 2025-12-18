8731ee7c382068f29ae5315e583e306dbb51ee4c
CodingRanjith <ranjith.c96me@gmail.com>
2025-12-18 13:39:44 +0530

    fdh

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,12 +1,12 @@
 
 
 // src/utils/api.js
-export const BASE_URL = 'https://worklogz.onrender.com';
+// export const BASE_URL = 'https://worklogz.onrender.com';
 
 // src/utils/api.js
 
 // // // // Base URL
-// export const BASE_URL = 'http://localhost:5000'; // Change to your live domain when needed'https://worklogz.onrender.com'
+export const BASE_URL = 'http://localhost:5000'; // Change to your live domain when needed'https://worklogz.onrender.com'
 
 // -----------------
 // Auth & User APIss
